06d10a79b431b7855a2856db5889210670f37941
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-09-20 17:08:05 +0530

    first data ingestion component updated

---
+++ src/components/stage01_data_ingestion.py
@@ -0,0 +1,30 @@
+#according to our data sources we write data_ingestion.py file code 
+#here we are getting data from url and in some other cases we get data from Databases, or from cloud storage like AWS S3, GCP, AZURE etc
+import os
+import urllib.request as request
+from zipfile import ZipFile
+from src.logger.logging import logging
+from src.exception import CustomException
+from pathlib import Path
+from tqdm import tqdm #library used to display smart progress bars for loops and iterations
+from src.entity import DataIngestionConfig 
+from src.utils import utils
+
+class DataIngestion:
+    def __init__(self):
+        pass
+
+    def download_file(self):
+        pass
+
+    
+
+    def get_updated_list_of_files(self):
+        pass
+
+    def preprocess(self):
+        pass
+
+    def unzip_and_clean(self):
+        pass
+    
