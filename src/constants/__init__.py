ee9de11ffeb8a0974818a133e279acac779ae7ac
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-09-28 19:28:50 +0530

    constants updated for dataingestion

---
+++ src/constants/__init__.py
@@ -0,0 +1,7 @@
+# Constants for file paths
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("configs/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+ARTIFACTS_DIR = Path("artifacts")
+DATA_INGESTION_DIR = ARTIFACTS_DIR / "data_ingestion"
