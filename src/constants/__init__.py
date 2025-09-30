ffdbe4c43a983c5c6a09511e0f1e2b3561b4782a
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-09-30 14:10:34 +0530

    constants updated for base model stage

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -5,3 +5,4 @@ CONFIG_FILE_PATH = Path("configs/config.yaml")
 PARAMS_FILE_PATH = Path("params.yaml")
 ARTIFACTS_DIR = Path("artifacts")
 DATA_INGESTION_DIR = ARTIFACTS_DIR / "data_ingestion"
+PREPARE_BASE_MODEL_DIR = ARTIFACTS_DIR / "prepare_base_model"
