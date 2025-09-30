29d701de7da3007d7a0d0e64be4cb90993f27a41
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-09-30 14:12:11 +0530

    prepare base model component added

---
+++ src/components/stage02_prepare_base_model.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+from src.entity.config_entity import PrepareBaseModelConfig
+import tensorflow as tf
+
+
+
+class PrepareBaseModel:
+    def __init__(self,config:PrepareBaseModelConfig):
+        self.config = config
+
+    def get_base_model(self):
+        self.model = tf.keras.applications.vgg16.VGG16(
+            input_shape = self.config.params_image_size,
+            include_top = self.config.params_include_top,
+            weights = self.config.params_weights
+        )
+        self.save_model(path=self.config.base_model_path, model = self.model)
+    
+    @staticmethod
+    def _prepare_base_model(model, classes, freeze_all, freeze_till, learning_rate):
+        if freeze_all: # If freeze_all is True, freeze all layers in the base model
+            for layer in model.layers:
+                layer.trainable = False # Freeze the entire base model
+        elif (freeze_till is not None) and (freeze_till > 0): 
+            for layer in model.layers[:-freeze_till]:
+                layer.trainable = False  # Freeze layers up to the specified layer
+        # Add custom layers on top of the base model
+        flatten_in = tf.keras.layers.Flatten()(model.output) # Flatten the output of the base model
+        # Add a dense layer with softmax activation for classification
+        prediction = tf.keras.layers.Dense(units=classes, activation="softmax")(flatten_in)
+        # Create the final model
+        full_model = tf.keras.Model(inputs=model.input, outputs=prediction)
+        # Compile the model with Adam optimizer and categorical crossentropy loss
+        full_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
+                           loss=tf.keras.losses.CategoricalCrossentropy(),
+                           metrics=["accuracy"])
+        full_model.summary() # Print the model summary
+        return full_model
+
+
+    def update_base_model(self):
+        self.full_model = self._prepare_base_model(
+            model = self.model,
+            classes = self.config.params_classes,
+            freeze_all = True,
+            freeze_till = None,
+            learning_rate = self.config.params_learning_rate
+        )
+        self.save_model(path=self.config.updated_base_model_path, model = self.full_model)
+    
+    @staticmethod
+    def save_model(path:Path, model:tf.keras.Model):
+        model.save(path)
+        print(f"Model is saved at : {path}")
