d26bfa0610d682dd0d30967f20d51c54c3993933
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-09-30 17:04:08 +0530

    configurationManager-prepared for callbacks

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -3,8 +3,10 @@ from src.utils.utils import read_yaml, create_directory
 from src.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from src.entity.config_entity import (
     DataIngestionConfig,
-    PrepareBaseModelConfig
+    PrepareBaseModelConfig,
+    PrepareCallbacksConfig
 )
+import os
 class ConfigurationManager:
     def __init__(self,config_filepath=CONFIG_FILE_PATH,params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath) #config.yaml file read
@@ -39,7 +41,21 @@ class ConfigurationManager:
         return prepare_base_model_config
 
 
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config["prepare_callbacks"]
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directory([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
 
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir=Path(config["root_dir"]),
+            tensorboard_root_log_dir=Path(config["tensorboard_root_log_dir"]),
+            checkpoint_model_filepath=Path(config["checkpoint_model_filepath"])
+        )
+
+        return prepare_callback_config
 
 
 
